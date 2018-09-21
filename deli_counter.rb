7f836a05a84aeff8d57f7e9c34d3e3237bfdb7bf
Anuj Panchal <anujpan1@gmail.com>
2018-09-21 19:57:27 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,6 +18,11 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.index(name)+1} in line."
 end
 
+# def take_a_number(katz_deli, name)
+  # katz_deli.push(name)
+  # puts "Welcome, #{name}. You are number #{katz_deli.index(name)+1} in line."
+# end
+
 def now_serving(katz_deli)
   if katz_deli.any? == false
     puts "There is nobody waiting to be served!"
